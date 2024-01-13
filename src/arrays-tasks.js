1dada929d3c5051e2786e07e1d59ffb279e655a7
DenverDen <denverden@mail.ru>
2024-01-13 09:01:25 +0300

    feat: add result isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -137,8 +137,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((el) => arr[0].length === el.length);
 }
 
 /**
